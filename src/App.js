a3dfd8c400a48ec882d4e826fb6c0eb55a2e891c
Peter-Kibet <peter.kibet@hotmail.com>
2023-07-28 18:36:13 +0300

    Set up routng on my app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Categories from "./Categories";
+import BookList from "./components/BookList";
+import BookForm from "./components/BookForm";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React 18 Alpha</h1>
-    </div>
+    <Router>
+      <div className="app-container">
+        <nav className="navigation">
+          <Link to="/">Home</Link>
+          <Link to="/categories">Categories</Link>
+        </nav>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/categories" component={Categories} />
+      </div>
+    </Router>
   );
 }
 
