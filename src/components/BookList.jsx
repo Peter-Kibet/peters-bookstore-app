f58844467ccecf2a990b7fdecd0529893cf40a1e
Peter-Kibet <peter.kibet@hotmail.com>
2023-08-04 07:36:17 +0300

    Import useSelector in my BookList.jsx to access books state from redux store

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
-import PropTypes from 'prop-types'; // Import prop-types
+// import PropTypes from 'prop-types'; // Import prop-types
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import '../styles/BookList.css';
 
-const BookList = ({ books }) => (
-  <div className="bookList">
-    {books.map((book) => (
-      <Book key={book.id} book={book} />
-    ))}
-  </div>
-);
+// const BookList = ({ books }) => (
+//   <div className="bookList">
+//     {books.map((book) => (
+//       <Book key={book.id} book={book} />
+//     ))}
+//   </div>
+// );
 
-// Add propTypes to validate the expected props
-BookList.propTypes = {
-  books: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-      author: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
+// // Add propTypes to validate the expected props
+// BookList.propTypes = {
+//   books: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       title: PropTypes.string.isRequired,
+//       author: PropTypes.string.isRequired,
+//     }),
+//   ).isRequired,
+// };
+
+// export default BookList;
+
+const BookList = () => {
+  const books = useSelector((state) => state.books.books);
+
+  return (
+    <div className="bookList">
+      {books.map((book) => (
+        <Book key={book.id} book={book} />
+      ))}
+    </div>
+  );
 };
 
 export default BookList;
