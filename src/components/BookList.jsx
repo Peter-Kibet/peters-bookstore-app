206d08f87599ff91210cdf2303722b0d95118abc
Peter-Kibet <peter.kibet@hotmail.com>
2023-08-04 07:53:30 +0300

    Refactoredmy BookForm.jsx to remove all the props and import useDispatch to disbatch addBook action

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -4,27 +4,6 @@ import { useSelector } from 'react-redux';
 import Book from './Book';
 import '../styles/BookList.css';
 
-// const BookList = ({ books }) => (
-//   <div className="bookList">
-//     {books.map((book) => (
-//       <Book key={book.id} book={book} />
-//     ))}
-//   </div>
-// );
-
-// // Add propTypes to validate the expected props
-// BookList.propTypes = {
-//   books: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.number.isRequired,
-//       title: PropTypes.string.isRequired,
-//       author: PropTypes.string.isRequired,
-//     }),
-//   ).isRequired,
-// };
-
-// export default BookList;
-
 const BookList = () => {
   const books = useSelector((state) => state.books.books);
 
