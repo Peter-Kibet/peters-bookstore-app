661cfa3fc24bb54052955a39900128a15b948a56
Peter-Kibet <peter.kibet@hotmail.com>
2023-08-05 19:46:26 +0300

    disbatch the fetchBooks thunk after components are rendered to fetch books

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchBooks } from '../redux/books/booksSlice';
 import Book from './Book';
 import '../styles/BookList.css';
 
 const BookList = () => {
+  const dispatch = useDispatch();
   const books = useSelector((state) => state.books.books);
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
 
   return (
     <div className="bookList">
