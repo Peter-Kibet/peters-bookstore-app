ea919fdd5510b6c50299cb9ba16409fa66f62658
Peter-Kibet <peter.kibet@hotmail.com>
2023-08-05 19:40:43 +0300

    create async Thunks for API Calls

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,26 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// Replace the base URL with the actual base URL of the Bookstore API
+const baseURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
+
+// Async thunk for fetching books from the API
+export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
+  const response = await axios.get(`${baseURL}/apps/abc123/books`);
+  return response.data;
+});
+
+// Async thunk for adding a book to the API
+export const addBookToAPI = createAsyncThunk(
+  'books/addBook',
+  async (bookData) => {
+    const response = await axios.post(`${baseURL}/apps/abc123/books`, bookData);
+    return response.data;
+  },
+);
+
+// Async thunk for removing a book from the API
+export const removeBookFromAPI = createAsyncThunk(
+  'books/removeBook',
+  async (itemId) => {
+    await axios.delete(`${baseURL}/apps/abc123/books/${itemId}`);
+    return itemId;
+  },
+);
 
 const initialState = {
-  books: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
+  books: [],
 };
 
 const booksSlice = createSlice({
@@ -41,6 +50,20 @@ const booksSlice = createSlice({
     removeBook: (state, action) => {
       state.books = state.books.filter((book) => book.id !== action.payload);
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBooks.fulfilled, (state, action) => {
+        state.books = action.payload;
+      })
+      .addCase(addBookToAPI.fulfilled, (state, action) => {
+        state.books.push(action.payload);
+      })
+      .addCase(removeBookFromAPI.fulfilled, (state, action) => {
+        state.books = state.books.filter(
+          (book) => book.item_id !== action.payload,
+        );
+      });
   },
 });
 
