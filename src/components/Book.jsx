83332ba6a6766d43431522625a61bb805a07cfc4
Peter-Kibet <peter.kibet@hotmail.com>
2023-08-05 19:58:13 +0300

    Fix the bookList

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import '../styles/Book.css';
@@ -8,20 +8,12 @@ const Book = ({ itemId }) => {
   const dispatch = useDispatch();
   const book = useSelector((state) => state.books.books.find((book) => book.item_id === itemId));
 
-  const [isDeleted, setIsDeleted] = useState(false);
-
   const handleDelete = () => {
     // Dispatch the removeBook action to update the Redux store
     dispatch(removeBook(itemId));
-    setIsDeleted(true);
   };
 
-  if (!book) {
-    // Book not found, handle the case gracefully
-    return <div>Book not found.</div>;
-  }
-
-  return !isDeleted ? (
+  return (
     <div className="books">
       <span>
         {book.title}
@@ -30,12 +22,11 @@ const Book = ({ itemId }) => {
         {book.author}
       </span>
       <span className="category">{book.category}</span>
-
       <button type="button" onClick={handleDelete}>
         Delete
       </button>
     </div>
-  ) : null;
+  );
 };
 
 Book.propTypes = {
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -7,10 +7,19 @@ import '../styles/BookList.css';
 const BookList = () => {
   const dispatch = useDispatch();
   const books = useSelector((state) => state.books.books);
+  const status = useSelector((state) => state.books.status);
 
   useEffect(() => {
     dispatch(fetchBooks());
   }, [dispatch]);
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  if (status === 'failed') {
+    return <div>Error fetching books</div>;
+  }
 
   return (
     <div className="bookList">
